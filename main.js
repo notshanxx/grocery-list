7c23dad17b98823f786ab0d151bca0c8eabd7694
Shanxx <96603980+notshanxx@users.noreply.github.com>
2022-08-24 11:38:05 +0800

    Add local storage functions and animations on load

--- main.js
+++ main.js
@@ -2,10 +2,73 @@ const input = document.querySelector("input")
 const saveBtn = document.querySelector("#save-btn")
 const pEl = document.querySelector("p")
 const list = document.querySelector("#list")
+
+// Local Storage
+let savedList = []
+let checkedList = []
+
+let lastEnteredListFromLocalStorage  = JSON.parse(localStorage.getItem("last-entered-list"))
+
+let lastCheckedListFromLocalStorage  = JSON.parse(localStorage.getItem("last-checked-list"))
+
+
+if(lastEnteredListFromLocalStorage && lastCheckedListFromLocalStorage){
+ savedList = lastEnteredListFromLocalStorage
+ checkedList = lastCheckedListFromLocalStorage
+}
+
+
+
+
+
+
+
+
+//retrieve last entered list and checked list from local Storage
+
+window.onload = () => {
+  
+  for (var i = 0; i < savedList.length; i++) {
+    
+    add(savedList[i])
+    
+    
+    console.log(savedList[i])
+    
+  }
+  
+  
+  setTimeout( () => {
+    
+    for (var i = 0; i < checkedList.length; i++) {
+    
+      if (checkedList[i] === true) {
+    
+        list.children[i].classList.add("checked")
+    
+        console.log("checked")
+    
+      } else if (checkedList[i] === false) {
+    
+        list.children[i].classList.remove("checked")
+        console.log("unchecked")
+    
+      }
+  }}, 700 )
+  
+  
+  
+}
+
+
+
+// LISTEN FOR CLICK
 saveBtn.addEventListener('click', function(){
-  // check if input have value
-  if(input.value){
-    add()
+  // check if input have the same value
+  if(savedList.includes(input.value)){
+    alert("duplicate list")
+  }else if(input.value){
+    add(input.value)
   }
 }
 )
@@ -13,7 +76,7 @@ saveBtn.addEventListener('click', function(){
 // Adding the list when clicked enter key
 document.addEventListener("keyup", ({key}) => {
     if (key === "Enter" && input.value) {
-      add()
+      add(input.value)
     }
 })
 
@@ -24,8 +87,9 @@ document.addEventListener("keyup", ({key}) => {
 // Div = the list items container
 // CheckBtn = the check button
 // Delete Btn = the delete button
+// inputValue = input.value
 
-function add(){
+function add(inputValue){
   
   let createH3 = document.createElement("h3")
   let createDiv = document.createElement("div")
@@ -34,26 +98,86 @@ function add(){
   
   createCheckBtn.src = "check.svg"
   createCheckBtn.setAttribute("id", "check-btn")
-  createCheckBtn.addEventListener('click', () => {
-    createDiv.classList.toggle("checked")
-  })
   
   createDeleteBtn.src = "trash-2.svg"
   createDeleteBtn.setAttribute("id", "delete-btn")
+  
+  createH3.textContent = inputValue
+  
+  // save to LocalStorage
+  saveToSavedList()
+  
+  
+  
+  
+  
+  // DELETE FUNCTIONS
+  
   createDeleteBtn.addEventListener("click", () =>{ 
     
+    // Delete the list from local storage
+      let index = savedList.indexOf(createH3.textContent)
+      savedList.splice(index, 1)
+      checkedList.splice(index, 1)
+      localStorage.removeItem("last-entered-list")
+      localStorage.removeItem("last-checked-list")
+      
+      localStorage.setItem("last-checked-list", JSON.stringify(checkedList))
+      localStorage.setItem("last-entered-list", JSON.stringify(savedList))
+      
     
-    // Works like magic Delete the list on clicking delete btn
     
-    createDiv.style.animation = "deleteanimation .5s cubic-bezier(0.5,0.3,0.3,0.5)"
+    
+    // Delete the list on clicking delete btn
+    
+    createDiv.style.animation = "deleteanimation 1s cubic-bezier(0.5,0.3,0.3,0.5)"
     
     // Delay .5s when deleting making animation first
     
     setTimeout(() => createDeleteBtn.parentNode.remove(), 500)
+  //  console.log(createH3.value)
+   // console.log(createDeleteBtn.parentNode.createH3.value)
+    
+
+    
     
   })
   
-  createH3.textContent = input.value
+  
+  
+  
+  
+  
+  
+  //  CHECK FUNCTIONS
+  
+    createCheckBtn.addEventListener('click', () => {
+    
+      
+      let index = savedList.indexOf(createH3.textContent)
+      
+      console.log(savedList.includes(createH3.textContent))
+       console.log(index)
+       
+      if (checkedList[index] === false) {
+        checkedList.splice(index, 1, true)
+        localStorage.removeItem("last-checked-list")
+        localStorage.setItem("last-checked-list", JSON.stringify(checkedList))
+        createDiv.classList.add("checked")
+      }else if(checkedList[index] === true){
+        checkedList.splice(index, 1, false)
+        localStorage.removeItem("last-checked-list")
+        localStorage.setItem("last-checked-list", JSON.stringify(checkedList))
+        createDiv.classList.remove("checked")
+      }
+    
+    
+    
+  })
+
+  
+  
+  
   //clear input
   input.value = ""
   createH3.setAttribute("id", "list")
@@ -63,6 +187,23 @@ function add(){
   createDiv.appendChild(createH3)
   createDiv.appendChild(createDeleteBtn)
   list.appendChild(createDiv)
+  
 }
+
+
+function saveToSavedList() {
+  
+  if(input.value){
+  
+   savedList.push(input.value)
+   localStorage.setItem("last-entered-list", JSON.stringify(savedList))
+   
+   checkedList.push(false)
+   localStorage.setItem("last-checked-list", JSON.stringify(checkedList))
+}
+
+}
+
+
 
 
